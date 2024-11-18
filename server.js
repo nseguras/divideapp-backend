c65c44d71b1e5c5d989abeb1806de3ddda0e03de
Nicolás Segura <nseg97@gmail.com>
2024-11-18 19:18:13 -0300

    Agregar gitignore

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -23,5 +23,5 @@ app.post('/divide', (req, res) => {
 
 // Iniciar servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
